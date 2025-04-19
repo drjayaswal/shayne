06724e92a85423f82e889d006cad091f092fcd38
Dhruv Ratan Jayaswal <mrdhruv.professional@gmail.com>
2025-04-19 12:15:04 +0530

    compiler.js

---
+++ compiler.js
@@ -0,0 +1,13 @@
+import {lexer} from './lexer.js';
+import {parser} from './parser.js';
+import keywords from './Keywords.js';
+import operators from './Operators.js';
+import delimiters from './Delimiters.js';
+import dataTypes from './DataTypes.js';
+
+export function compiler(input) { 
+    const token = lexer(input);
+    console.table(token);
+    const ast = parser(token);
+    console.dirxml(ast);
+}
